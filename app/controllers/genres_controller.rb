195f337f50ee93ef699124685414da52f225732d
Alejandro Alvarado <alexconh@hotmail.com>
2016-08-12 11:35:03 -0500

    Genres controller fixed, syntax of search changed to find()

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -6,11 +6,7 @@ class GenresController < ApplicationController
   def browse
   	if params.include? :id
 
-      # Este comando es el que viene en el tutorial, pero no funciona
-  		@genre = Genre.find(:first, :conditions => ['id = ?', params[:id]])
-
-      # También intenté con este comando pero tampoco jaló, los probé por separado, comentados y sin comentarios
-      # @genre = Genre.find_by_id(params[:id])
+      @genre = Genre.find(params[:id])
   	else
   		redirect_to :action => 'index'
   	end
